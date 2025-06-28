ff9ce738f84ead89bbe5de3279ebf4fe09db2e68
Chandrarit <130826166+crm16@users.noreply.github.com>
2025-06-28 20:56:08 +0530

    Multiplication_table.java

--- Multiplication_table.java
+++ Multiplication_table.java
@@ -7,10 +7,11 @@ public class Multiplication_table {
         int num = sc.nextInt();
         System.out.print("Enter range: ");
         int range = sc.nextInt();
-        System.out.println("Table of " + num + ": ");
+        
+        System.out.println("\nTable of " + num + ": ");
 
-        for(int i=0 ; i<range ; i++){
-            System.out.println( num + " X " + (i+1) + " = " + (num*(i+1)));
+        for(int i=0 ; i<=range ; i++){
+            System.out.println( num + " X " + (i) + " = " + (num*i));
         }
     }
 }
